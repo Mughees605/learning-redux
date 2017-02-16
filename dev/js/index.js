ea2d4be0a532867ee18af19bee5ebe7c69efd846
Hafiz Mughees <mughees605@gmail.com>
2017-02-16 08:11:18 +0500

    adding create store, store is the all of the data of the application

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,9 +1,11 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
+import {createStore} from "redux"
 
 
 
+const store = createStore();
 ReactDOM.render(
     <h1>check this is</h1>
     ,
